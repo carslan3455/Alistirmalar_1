5eddd41455636738d9859ac4db9f3a773a48f5cb
carslan3455 <carslan3455@gmail.com>
2021-01-26 17:31:55 +0100

    Add project files.

---
+++ Alistirmalar_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             1) Dictionary sinifi icinde sizin belirleyeceginiz bir key e uygun olarak deger saklar
+             2) key turunun belirlenmesi sarttir
+              
+            */
+            Dictionary<string, int> AdYas = new Dictionary<string, int>();
+            // veri ekliyoruz 
+
+            AdYas.Add("Sinem", 2011);
+            AdYas.Add("Ihsan", 2008);
+            AdYas.Add("Arkadas", 2007);
+
+            foreach (var kisi in AdYas)
+            {
+                Console.WriteLine(kisi);
+            }
+
+            // deger sayma ozelligi var
+            int elemanSayisi = AdYas.Count;
+            Console.WriteLine();
+            Console.WriteLine("Eleman sayisi : " + elemanSayisi);
+
+            // silme ozelligi var
+            AdYas.Remove("Arkadas");
+            Console.WriteLine();
+
+            foreach (var kisi in AdYas)
+            {
+                Console.WriteLine(kisi);
+            }
+
+            Console.WriteLine("Eleman sayisi : " + AdYas.Count);
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenciNoAd = new MyDictionary<int, string>();
+            ogrenciNoAd.Add(1, "Sinem");
+            ogrenciNoAd.Add(2, "Ihsan");
+            ogrenciNoAd.Add(3, "Es");
+            ogrenciNoAd.Add(4, "Oz");
+
+            ogrenciNoAd.Print(ogrenciNoAd);
+
+        }
+    }
+
+    class MyDictionary<K, V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] tempKeys = keys;
+            V[] tempValues = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+        public void Print(MyDictionary<K, V> myDictionary)
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("[" + keys[i] + "," + values[i] + "]");
+
+            }
+        }
+
+    }
+}

6c1170dfa03194b5d019331de2d13cd968ea4820
carslan3455 <carslan3455@gmail.com>
2021-01-27 14:41:56 +0100

    Interface_2

---
+++ Inheritance/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[] 
+            {
+            new Customer{FirstName = "Sinem" }, 
+            new Student {FirstName = "Ihsan" },
+            new Person {FirstName = "Ess" } 
+            };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person  // Person da bir varliktir. new yapilir. Interface ler soyuttur new yapilamaz
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    interface IPerson
+    {
+
+    }
+
+    interface IPerson2
+    {
+
+    }
+
+    class Customer:Person ,IPerson,IPerson2  
+        // bir varlik 1 tane Inheritance alabilir. birden fazla Interface alabilir
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Depertmant { get; set; }
+    }
+}
---
+++ Interface_2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Interface_2
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            // Interface new lenemez
+            // IPersonManager personManager = new IPersonManager();
+
+            //IPersonManager customerManager = new CustomerManager();
+            //customerManager.Add();
+            //customerManager.Update();
+
+            //IPersonManager employeeManager = new EmployeeManager();
+            //employeeManager.Add();
+            //employeeManager.Update();
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new CustomerManager());
+            projectManager.Add(new EmployeeManager());
+            projectManager.Add(new InternManager());   // Stajyer methodu sonradan eklenmesine ragmen sistem bozulmadi
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IPersonManager
+    {
+        // unimplemented operation - Gövdesi olmayan method
+        void Add();
+        void Update();
+    }
+
+    //inherits -> class larda   -------    implements -> interface
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            // müsteri ekleme kodlari yazilir
+            Console.WriteLine("Müsteri eklendi...");
+        }
+
+        public void Update()
+        {
+            // müsteri guncelleme kodlari yazilir
+            Console.WriteLine("Müsteri güncellendi...");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            // calisan ekleme kodlari yazilir
+            Console.WriteLine("Calisan eklendi...");
+        }
+        public void Update()
+        {
+            // calisan guncelleme kodlari yazilir
+            Console.WriteLine("Calisan güncellendi...");
+        }
+    }
+
+    class InternManager : IPersonManager    // Intern -> Stajyer
+    {
+        public void Add()
+        {
+            // Stajyer ekleme kodlari yazilir
+            Console.WriteLine("Stajyer eklendi...");
+        }
+        public void Update()
+        {
+            // Stajyer guncelleme kodlari yazilir
+            Console.WriteLine("Stajyer güncellendi...");
+        }
+    }
+    class ProjectManager
+    {
+        public void Add(IPersonManager person)
+        {
+            person.Add();
+        }
+    }
+}
